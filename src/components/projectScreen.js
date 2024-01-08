8d5aa1eca56047a0802056e0606be27092eeb105
filippopisano99 <xpisi1999@gmail.com>
2024-01-08 20:12:58 +0100

    Mobile view

--- src/components/projectScreen.js
+++ src/components/projectScreen.js
@@ -2,12 +2,12 @@ import Image from 'next/image'
 
 export default function Projectscreen() {
     return (
-        <section className='
+        <div className='
         w-full h-screen
         flex flex-row
         justify-center'>
         <pre>To Do...</pre>
-      </section>
+      </div>
         
     )
 }
--- src/components/splashscreen.js
+++ src/components/splashscreen.js
@@ -2,34 +2,56 @@ import Image from 'next/image'
 
 export default function Splashscreen() {
     return (
-        <section className='
-        w-full h-screen
-        flex flex-row
-        justify-center'>
-
         <div className='
-          pr-36 pt-96'
-          >
-          
-          <span className="text-white text-6xl font-semibold">Ciao 👋🏻 sono </span>
-          <span className="text-primary text-6xl font-semibold">Filippo</span>
-          <span className="text-white text-6xl font-semibold">, <br/>
-          Coffe lover, Tech nerd e
-          <br/>
-          Software developer
-          </span>
+        w-full h-screen
+        items-center
+        justify-center
+        
+        md:flex md:flex-row-reverse        
+        '>
 
-
-        </div>
-        <div className='pt-10'>
-          <div className="avatar">
+        <div className='flex justify-center'>
+          <div className="avatar 
+          pb-3
+          md:w-80 w-2/4
+          md:pb-48
+          ">
             <div className="bg-primary rounded-full">
               <img src="avatar.png" />
             </div>
           </div>
         </div>
 
-      </section>
+        <div className='
+          flex flex-col items-center
+          md:pt-20
+          md:pr-20 '
+          >
+          
+          <div>
+            <span className="
+            text-white 
+            md:text-5xl text-2xl
+            font-semibold">Ciao 👋🏻 sono </span>
+            <span className="
+            text-primary
+            md:text-5xl text-2xl
+            font-semibold">Filippo</span>
+            <span className="
+            text-white 
+            md:text-5xl text-2xl
+            font-semibold">, <br/>
+            Coffe lover, Tech nerd e
+            <br/>
+            Software developer
+            </span>
+          </div>
+
+
+        </div>
+        
+
+      </div>
         
     )
 }
